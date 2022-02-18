185b5db426e6868f58d8c8bd4ff32d0140e577ad
scriptlamp <scriptlamp@gmail.com>
2022-02-18 18:10:03 +0530

    Changes of Sort function for contact list

--- src/app/pipe/sort.pipe.ts
+++ src/app/pipe/sort.pipe.ts
@@ -7,28 +7,20 @@ export type SortOrder = 'asc' | 'desc';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: any[], sortOrder: SortOrder | string = 'asc', sortKey?: string): any {
+  transform(value: any[], sortOrder: SortOrder | string = 'asc', sortKey: string): any {
     sortOrder = sortOrder && (sortOrder.toLowerCase() as any);
 
     if (!value || (sortOrder !== 'asc' && sortOrder !== 'desc')) return value;
 
-    let numberArray = [];
     let stringArray = [];
-
-    if (!sortKey) {
-      numberArray = value.filter(item => typeof item === 'number').sort();
-      stringArray = value.filter(item => typeof item === 'string').sort();
-    } else {
-      numberArray = value.filter(item => typeof item[sortKey] === 'number').sort((a, b) => a[sortKey] - b[sortKey]);
-      stringArray = value
-        .filter(item => typeof item[sortKey] === 'string')
-        .sort((a, b) => {
-          if (a[sortKey].toLowerCase() < b[sortKey].toLowerCase()) return -1;
-          else if (a[sortKey].toLowerCase() > b[sortKey].toLowerCase()) return 1;
-          else return 0;
-        });
-    }
-    const sorted = numberArray.concat(stringArray);
+    stringArray = value
+      .sort((a, b) => {
+        if (a[sortKey].toLowerCase() < b[sortKey].toLowerCase()) return -1;
+        else if (a[sortKey].toLowerCase() > b[sortKey].toLowerCase()) return 1;
+        else return 0;
+      });
+    
+    const sorted = stringArray;
     return sortOrder === 'asc' ? sorted : sorted.reverse();
   }
 }
